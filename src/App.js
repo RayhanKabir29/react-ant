7976a2414ec77ca777a6e40204609234b518516b
rayhan kabir <rayhan.kabir29@gmail.com>
2023-05-16 10:49:14 +0600

    icon package added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import "antd/dist/reset.css";
 import { Button } from "antd";
+import { useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const handleBtnClic = (e) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  };
   return (
     <div className="App">
-      <Button  type="primary">Explore AntDesign</Button>
+      <Button type="primary" loading={loading} onClick={handleBtnClic}>
+        Explore AntDesign
+      </Button>
     </div>
   );
 }
